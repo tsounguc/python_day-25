5f5928f91f830bd0051cca34a38266c4e5b41d99
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2023-10-21 23:05:28 -0400

    read csv file without and with importing csv package

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
+# CSV stands for Comma Separated Values
+import csv
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# with open("weather_data.csv") as data_file:
+#     data = data_file.readlines()
+#     print(data)
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+with open("weather_data.csv") as data_file:
+    data = csv.reader(data_file)
+    temperatures = []
+    for row in data:
+        if 'day' not in row:
+            temperature = int(row[1])
+            temperatures.append(temperature)
+    print(temperatures)
