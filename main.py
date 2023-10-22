4ae53cc22b933b482c5c655458f50a5176e68101
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2023-10-21 23:20:49 -0400

    read csv file using the pandas library

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 # CSV stands for Comma Separated Values
-import csv
+# import csv
 
 # with open("weather_data.csv") as data_file:
 #     data = data_file.readlines()
 #     print(data)
 
-with open("weather_data.csv") as data_file:
-    data = csv.reader(data_file)
-    temperatures = []
-    for row in data:
-        if 'day' not in row:
-            temperature = int(row[1])
-            temperatures.append(temperature)
-    print(temperatures)
+# with open("weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperatures = []
+#     for row in data:
+#         if 'day' not in row:
+#             temperature = int(row[1])
+#             temperatures.append(temperature)
+#     print(temperatures)
+
+import pandas
+
+data = pandas.read_csv("weather_data.csv")
+# print(data)
+print(data["temp"])
